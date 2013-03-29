7f2ea0668be1a8fc56c3bd9c9a31df11401f627e
Paul Belanger <paul.belanger@polybeacon.com>
2013-03-29 17:09:32 -0400

    Initial commit
    Signed-off-by: Paul Belanger <paul.belanger@polybeacon.com>

---
+++ doc/source/conf.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+
+# Copyright (C) 2013 PolyBeacon, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+# implied.
+
+import os
+import sys
+
+from stripe.version import STRIPE_VERSION as stripe_version
+
+
+extensions = ['sphinx.ext.autodoc']
+templates_path = ['_templates']
+source_suffix = '.rst'
+master_doc = 'index'
+project = u'Stripe'
+copyright = u'2013, Paul Belanger'
+release = stripe_version.version_string_with_vcs()
+version = stripe_version.canonical_version_string()
+exclude_patterns = []
+pygments_style = 'sphinx'
+html_theme = 'default'
+htmlhelp_basename = 'stripeedoc'
+
+latex_elements = {
+}
+
+latex_documents = [
+  ('index', 'Stripe.tex', u'Stripe Documentation',
+   u'Paul Belanger', 'manual'),
+]
+
+man_pages = [
+    ('index', 'Stripe', u'Stripe Documentation',
+     [u'Paul Belanger'], 1)
+]
+
+texinfo_documents = [
+  ('index', 'Stripe', u'Stripe Documentation',
+   u'Paul Belanger', 'Stripe', 'One line description of project.',
+   'Miscellaneous'),
+]
---
+++ setup.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+
+# Copyright (C) 2013 PolyBeacon, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+# implied.
+
+import setuptools
+
+from stripe.openstack.common import setup
+from stripe.version import STRIPE_VERSION as version
+
+
+requires = setup.parse_requirements()
+test_requires = setup.parse_requirements(['tools/test-requires'])
+depend_links = setup.parse_dependency_links()
+
+setuptools.setup(
+    name='stripe',
+    version=version.canonical_version_string(always=True),
+    author='Paul Belanger',
+    author_email='paul.belanger@polybeacon.com',
+    url='https://github.com/kickstandproject/stripe',
+    packages=setuptools.find_packages(exclude=['tests', 'tests.*']),
+    include_package_data=True,
+    cmdclass=setup.get_cmdclass(),
+    install_requires=requires,
+    setup_requires=['setuptools_git>=0.4'],
+    dependency_links=depend_links,
+    zip_safe=False
+)
