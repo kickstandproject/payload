6fddf25ded9cf8b517b6d8f6578e0208cafe949c
Paul Belanger <paul.belanger@polybeacon.com>
2013-08-02 00:22:09 -0400

    Properly display license header in source files
    Change-Id: I570793398bbd1bbbbedafc9e37d83a4f29b8678f
    Signed-off-by: Paul Belanger <paul.belanger@polybeacon.com>

--- stripe/api/middleware.py
+++ stripe/api/middleware.py
@@ -11,10 +11,10 @@
 #      http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
-# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
-# License for the specific language governing permissions and limitations
-# under the License.
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 """
 Middleware to replace the plain text message body of an error
 response with one formatted so the client can parse it.
--- stripe/common/config.py
+++ stripe/common/config.py
@@ -13,15 +13,15 @@
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
-# implied.
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
 """
 Routines for configuring Stripe
 """
 
 from oslo.config import cfg
-
 
 from stripe.common import paths
 from stripe.openstack.common.db.sqlalchemy import session as db_session
