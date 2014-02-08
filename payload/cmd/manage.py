a1166220294fd5ca5e474575a152f35ac16ccef1
Paul Belanger <paul.belanger@polybeacon.com>
2014-02-08 15:04:29 -0500

    Rework payload-manage
    We are now able to leverage migrations properly.
    
    Change-Id: Ieb3d71d4526c25566007b21fc9b611e38a63149b
    Signed-off-by: Paul Belanger <paul.belanger@polybeacon.com>

--- payload/cmd/manage.py
+++ payload/cmd/manage.py
@@ -1,17 +1,12 @@
 # vim: tabstop=4 shiftwidth=4 softtabstop=4
 
-# Copyright (c) 2011 X.commerce, a business unit of eBay Inc.
-# Copyright 2010 United States Government as represented by the
-# Administrator of the National Aeronautics and Space Administration.
 # Copyright (C) 2013 PolyBeacon, Inc.
 #
-# All Rights Reserved.
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
 #
-# Licensed under the Apache License, Version 2.0 (the "License"); you may
-# not use this file except in compliance with the License. You may obtain
-# a copy of the License at
-#
-#      http://www.apache.org/licenses/LICENSE-2.0
+#    http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
@@ -19,155 +14,61 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# Interactive shell based on Django:
-#
-# Copyright (c) 2005, the Lawrence Journal-World
-# All rights reserved.
-#
-# Redistribution and use in source and binary forms, with or without
-# modification, are permitted provided that the following conditions are met:
-#
-#     1. Redistributions of source code must retain the above copyright notice,
-#        this list of conditions and the following disclaimer.
-#
-#     2. Redistributions in binary form must reproduce the above copyright
-#        notice, this list of conditions and the following disclaimer in the
-#        documentation and/or other materials provided with the distribution.
-#
-#     3. Neither the name of Django nor the names of its contributors may be
-#        used to endorse or promote products derived from this software without
-#        specific prior written permission.
-#
-# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
-# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
-# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
-# A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
-# OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
-# SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
-# LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
-# DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
-# THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
-# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+"""
+  CLI interface for payload management.
+"""
 
-import sys
+import logging
 
 from oslo.config import cfg
 
 from payload.common import config
-# TODO(pabelanger): Why can't we call payload.db directly?
-from payload.db.sqlalchemy import migration
+from payload.db import migration as db_migration
 from payload.openstack.common import log
-from payload import version
 
 CONF = cfg.CONF
+LOG = log.getLogger(__name__)
 
 
-# Decorators for actions
-def args(*args, **kwargs):
-    def _decorator(func):
-        func.__dict__.setdefault('args', []).insert(0, (args, kwargs))
-        return func
-    return _decorator
+def do_db_version():
+    """Print database's current migration level."""
+    print(db_migration.db_version())
 
 
-class DBCommands(object):
-    """Class for managing the database."""
-
-    def __init__(self):
-        pass
-
-    def sync(self):
-        """Sync the database up to the most recent version."""
-        return migration.db_sync()
-
-    def version(self):
-        """Print the current database version."""
-        print migration.db_version()
-
-
-CATEGORIES = {
-    'db': DBCommands,
-}
-
-
-def methods_of(obj):
-    """Get all callable methods of an object that don't start with underscore
-    returns a list of tuples of the form (method_name, method)
+def do_db_sync():
+    """Place a database under migration control and upgrade,
+    creating first if necessary.
     """
-    result = []
-    for i in dir(obj):
-        if callable(getattr(obj, i)) and not i.startswith('_'):
-            result.append((i, getattr(obj, i)))
-    return result
+    db_migration.db_sync(CONF.command.version)
 
 
 def add_command_parsers(subparsers):
-    for category in CATEGORIES:
-        command_object = CATEGORIES[category]()
+    parser = subparsers.add_parser('db-version')
+    parser.set_defaults(func=do_db_version)
 
-        parser = subparsers.add_parser(category)
-        parser.set_defaults(command_object=command_object)
+    parser = subparsers.add_parser('db-sync')
+    parser.set_defaults(func=do_db_sync)
+    parser.add_argument('version', nargs='?')
+    parser.add_argument('current_version', nargs='?')
 
-        category_subparsers = parser.add_subparsers(dest='action')
+    parser.add_argument(
+        '-g', '--granularity', default='days',
+        choices=['days', 'hours', 'minutes', 'seconds'],
+        help='Granularity to use for age argument, defaults to days.')
 
-        for (action, action_fn) in methods_of(command_object):
-            parser = category_subparsers.add_parser(action)
-
-            action_kwargs = []
-            for args, kwargs in getattr(action_fn, 'args', []):
-                parser.add_argument(*args, **kwargs)
-
-            parser.set_defaults(action_fn=action_fn)
-            parser.set_defaults(action_kwargs=action_kwargs)
-
-
-CATEGORY_OPT = cfg.SubCommandOpt(
-    'category', title='Command categories', handler=add_command_parsers
-)
-
-
-def get_arg_string(args):
-    arg = None
-    if args[0] == '-':
-    # (Note)zhiteng: args starts with FLAGS.oparser.prefix_chars
-    # is optional args. Notice that cfg module takes care of
-    # actual ArgParser so prefix_chars is always '-'.
-        if args[1] == '-':
-            # This is long optional arg
-            arg = args[2:]
-        else:
-            arg = args[3:]
-    else:
-        arg = args
-
-    return arg
-
-
-def fetch_func_args(func):
-    fn_args = []
-    for args, kwargs in getattr(func, 'args', []):
-        arg = get_arg_string(args[0])
-        fn_args.append(getattr(CONF.category, arg))
-
-    return fn_args
+command_opt = cfg.SubCommandOpt('command',
+                                title='Commands',
+                                help='Available commands',
+                                handler=add_command_parsers)
 
 
 def main():
-    CONF.register_cli_opt(CATEGORY_OPT)
-    script_name = sys.argv[0]
-    if len(sys.argv) < 2:
-        print ("\npayload version: %(version)s\n"
-               % {'version': version.VERSION_INFO.version_string_with_vcs()})
-        print script_name + " category action [<args>]"
-        print "Available categories:"
-        for category in CATEGORIES:
-            print "\t%s" % category
-        sys.exit(2)
-
+    CONF.register_cli_opt(command_opt)
     config.parse_args()
     log.setup('payload')
+    CONF.log_opt_values(LOG, logging.INFO)
 
-    fn = CONF.category.action_fn
-    fn_args = fetch_func_args(fn)
-    fn(*fn_args)
+    try:
+        CONF.command.func()
+    except Exception as e:
+        LOG.exception(e)
--- payload/common/utils.py
+++ payload/common/utils.py
@@ -16,30 +16,20 @@
 
 from oslo.config import cfg
 
-from payload.common import exception
-
 CONF = cfg.CONF
 
 
 class LazyPluggable(object):
     """A pluggable backend loaded lazily based on some value."""
 
-    def __init__(self, pivot, config_group=None, **backends):
+    def __init__(self, pivot, **backends):
         self.__backends = backends
         self.__pivot = pivot
         self.__backend = None
-        self.__config_group = config_group
 
     def __get_backend(self):
         if not self.__backend:
-            if self.__config_group is None:
-                backend_name = CONF[self.__pivot]
-            else:
-                backend_name = CONF[self.__config_group][self.__pivot]
-            if backend_name not in self.__backends:
-                msg = _('Invalid backend: %s') % backend_name
-                raise exception.PayloadException(msg)
-
+            backend_name = 'sqlalchemy'
             backend = self.__backends[backend_name]
             if isinstance(backend, tuple):
                 name = backend[0]
